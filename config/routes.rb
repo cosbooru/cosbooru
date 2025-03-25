091a0794c41ccca17c7ee34fd6fbce56f5085205
TypeA2 <nuanlim29@gmail.com>
2025-03-25 06:10:43 +0100

    remove AI metadata routes

--- config/routes.rb
+++ config/routes.rb
@@ -51,10 +51,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :ai_metadata, only: [:index, :show] do
-    get :stealth, on: :collection, to: "prompt_protector#index"
-  end
-  resources :ai_metadata_versions, only: [:index, :show]
+  # resources :ai_metadata, only: [:index, :show] do
+  #   get :stealth, on: :collection, to: "prompt_protector#index"
+  # end
+  # resources :ai_metadata_versions, only: [:index, :show]
   resources :api_keys, only: [:new, :create, :edit, :update, :index, :destroy]
 
   resources :artists do
@@ -203,13 +203,13 @@ Rails.application.routes.draw do
     resources :events, only: [:index], controller: "post_events", as: "post_events"
     resources :favorites, only: [:index, :create, :destroy]
     resources :replacements, :only => [:index, :new, :create], :controller => "post_replacements"
-    resource :ai_metadata, only: [:show] do
-      collection { put :create_or_update }
-      member do
-        put :revert
-        put :undo
-      end
-    end
+    # resource :ai_metadata, only: [:show] do
+    #  collection { put :create_or_update }
+    #  member do
+    #    put :revert
+    #    put :undo
+    #  end
+    # end
     resource :artist_commentary, only: [:show] do
       collection { put :create_or_update }
       member { put :revert }
