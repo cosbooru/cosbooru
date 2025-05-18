a6c68816a5a5ba61aa51a795a2fd0c3550b77283
TypeA2 <nuanlim29@gmail.com>
2025-05-18 11:43:01 +0200

    progress reporting in importing script

--- script/fixes/C001_import_aliases_implications.rb
+++ script/fixes/C001_import_aliases_implications.rb
@@ -6,15 +6,21 @@ require "json"
 
 # Import aliases and implications
 with_confirmation do
-  implications = JSON.parse(File.read("implications.json"))
-  puts "#{implications.size} implications"
-  implications.each do |impl|
-    TagImplication.approve!(antecedent_name: impl["antecedent_name"], consequent_name: impl["consequent_name"], approver: User.system)
-  end
+  system_user = User.system
 
-  aliases = JSON.parse(File.read("aliases.json"))
-  puts "#{aliases.size} aliases"
-  aliases.each do |impl|
-    TagAlias.approve!(antecedent_name: impl["antecedent_name"], consequent_name: impl["consequent_name"], approver: User.system)
+  which = ENV.fetch("WHICH", "user")
+
+  if which == "implications"
+    implications = JSON.parse(File.read("implications.json"))
+    implications.each_with_index  do |impl, idx|
+      puts "[#{idx} / #{implications.size}] imply #{impl["antecedent_name"]} -> #{impl["consequent_name"]}"
+      TagImplication.approve!(antecedent_name: impl["antecedent_name"], consequent_name: impl["consequent_name"], approver: system_user)
+    end
+  elsif which == "aliases"
+    aliases = JSON.parse(File.read("aliases.json"))
+    aliases.each_with_index  do |a, idx|
+      puts "[#{idx} / #{aliases.size}] alias #{a["antecedent_name"]} -> #{a["consequent_name"]}"
+      TagAlias.approve!(antecedent_name: a["antecedent_name"], consequent_name: a["consequent_name"], approver: system_user)
+    end
   end
 end
