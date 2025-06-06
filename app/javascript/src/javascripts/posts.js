a008a92d75f74d44df8734702d26e9ece429af2b
evazion <noizave@gmail.com>
2025-06-05 21:34:13 -0500

    js: replace Utility.test_max_width with isTouchscreen and isMobile.
    Split `Utility.test_max_width` into isTouchscreen for detecting whether we're using a touchscreen
    and isMobile for detecting whether we're using the mobile layout.
    
    Fixes it so that gestures work on tablets.

--- app/javascript/src/javascripts/posts.js
+++ app/javascript/src/javascripts/posts.js
@@ -1,6 +1,6 @@
 import CurrentUser from './current_user'
 import Utility from './utility'
-import { delay }  from './utility'
+import { delay, isMobile, isTouchscreen }  from './utility'
 import Notice from './notice'
 import Hammer from 'hammerjs'
 import Cookie from './cookie'
@@ -67,14 +67,11 @@ Post.initialize_info_tabs = function() {
 }
 
 Post.initialize_gestures = function() {
-  if (CurrentUser.data("disable-mobile-gestures")) {
+  if (!isTouchscreen() || CurrentUser.data("disable-mobile-gestures")) {
     return;
   }
   var $body = $("body");
   if ($body.data("hammer")) {
-    return;
-  }
-  if (!Utility.test_max_width(660)) {
     return;
   }
   $(".image-container").css({overflow: "visible"});
@@ -292,7 +289,7 @@ Post.initialize_post_preview_options_menu = function() {
 }
 
 Post.view_original = function(e = null) {
-  if (Utility.test_max_width(660)) {
+  if (isMobile()) {
     // Do the default behavior (navigate to image)
     return;
   }
@@ -312,7 +309,7 @@ Post.view_original = function(e = null) {
 }
 
 Post.view_large = function(e = null) {
-  if (Utility.test_max_width(660)) {
+  if (isMobile()) {
     // Do the default behavior (navigate to image)
     return;
   }
