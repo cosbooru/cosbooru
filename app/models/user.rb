42ab48329ce2504b8b9e857ad51f1e69b1a34022
evazion <noizave@gmail.com>
2025-07-06 00:32:25 -0500

    models: use builtin Rails normalizations instead of our own.
    Replace our own Normalizable concern and `normalize` method in favor of using the built-in Rails
    `normalizes` method. Also use `normalizes` instead of `before_validation` callbacks in various places.
    
    We also have to fix Searchable to not apply normalizations when using methods like `where_regex` or
    `where_like` because that can break the search. The trick is to use Arel::Node::Quoted to prevent
    normalization from occurring. See https://github.com/rails/rails/issues/53532 for details.
    
    https://api.rubyonrails.org/classes/ActiveRecord/Normalization/ClassMethods.html

--- app/models/user.rb
+++ app/models/user.rb
@@ -101,6 +101,8 @@ class User < ApplicationRecord
 
   attr_reader :password
 
+  normalizes :blacklisted_tags, with: ->(string) { string.to_s.lines.map(&:strip).join("\n") }
+
   after_initialize :initialize_attributes, if: :new_record?
   validates :name, user_name: true, on: :create
   validates :password, length: { minimum: 5 }, if: ->(rec) { rec.new_record? || rec.password.present? }
@@ -110,7 +112,6 @@ class User < ApplicationRecord
   validates :comment_threshold, inclusion: { in: (-100..5) }
   validate :validate_custom_css, if: :custom_style_changed?
   validate :validate_add_extra_data_attributes, unless: :new_record?
-  before_validation :normalize_blacklisted_tags
   before_create :promote_to_owner_if_first_user
 
   has_many :ai_metadata_versions, foreign_key: :updater_id, dependent: :destroy
@@ -579,11 +580,6 @@ class User < ApplicationRecord
 
     def rewrite_blacklist(old_name, new_name)
       blacklisted_tags.gsub!(/(?:^| )([-~])?#{Regexp.escape(old_name)}(?: |$)/i) { " #{$1}#{new_name} " }
-    end
-
-    def normalize_blacklisted_tags
-      return unless blacklisted_tags.present?
-      self.blacklisted_tags = blacklisted_tags.lines.map(&:strip).join("\n")
     end
 
     # @return [Array<String>] The list of blacklist rules. Each line in the blacklist is a rule.
