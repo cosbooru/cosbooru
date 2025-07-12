3dec8d5926d45132f1c03c77751877fc098866d7
asgembed <211016138+asgembed@users.noreply.github.com>
2025-07-12 20:39:28 +0800

    sources: add Mihuashi URL parser

--- app/logical/source/url.rb
+++ app/logical/source/url.rb
@@ -123,6 +123,7 @@ module Source
       Source::URL::CosplayerArchive,
       Source::URL::Marshmallow,
       Source::URL::Huashijie,
+      Source::URL::Mihuashi,
     ]
 
     # Parse a URL into a subclass of Source::URL, or raise an exception if the URL is not a valid HTTP or HTTPS URL.
