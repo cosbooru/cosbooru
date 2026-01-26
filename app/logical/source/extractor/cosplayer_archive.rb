c4da43e218a1dc589de8424d852ed2bf577c5708
CommentaryRequest <tele.didar.lol@gmail.com>
2026-01-26 19:07:23 +0500

    cosplayer_archive: add translated tags

--- app/logical/source/extractor/cosplayer_archive.rb
+++ app/logical/source/extractor/cosplayer_archive.rb
@@ -64,4 +64,20 @@ class Source::Extractor::CosplayerArchive < Source::Extractor
   def artist_commentary_desc
     page.at("span.black_mui14150")&.text
   end
+
+  def tags
+    return [] unless page.present?
+
+    page.search("a[href^='/photo_search.aspx?']").flat_map do |a|
+      uri = URI(a[:href])
+      params = CGI.parse(uri.query)
+      params.map do |key, values|
+        next unless %w[n2 n3 n4 n5 n6 n10].include?(key)
+        [
+          a.text.strip,
+          "https://cosp.jp#{a[:href]}"
+        ]
+      end
+    end.compact
+  end
 end
