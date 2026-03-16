danbooru/danbooru

rubocop: fix more rubocop warnings.

app/models/post_flag.rb
@@ -26,10 +26,12 @@ class PostFlag < ApplicationRecord
     rejected: 2,
   }
 
+  delegate :uploader_id, to: :post
+
   scope :by_users, -> { where.not(creator: User.system) }
   scope :by_system, -> { where(creator: User.system) }
-  scope :in_cooldown, -> { by_users.where("created_at >= ?", Danbooru.config.moderation_period.ago) }
-  scope :expired, -> { pending.where("post_flags.created_at < ?", Danbooru.config.moderation_period.ago) }
+  scope :in_cooldown, -> { by_users.where(created_at: Danbooru.config.moderation_period.ago..) }
+  scope :expired, -> { pending.where(post_flags: { created_at: ...Danbooru.config.moderation_period.ago }) }
   scope :active, -> { pending.or(rejected.in_cooldown) }
 
   module SearchMethods
@@ -93,12 +95,8 @@ class PostFlag < ApplicationRecord
 
     flag = post.flags.in_cooldown.last
     if !is_deletion && !creator.is_approver? && flag.present?
-      errors.add(:post, "cannot be flagged more than once every #{Danbooru.config.moderation_period.inspect} (last flagged: #{flag.created_at.to_formatted_s(:long)})")
+      errors.add(:post, "cannot be flagged more than once every #{Danbooru.config.moderation_period.inspect} (last flagged: #{flag.created_at.to_fs(:long)})")
     end
-  end
-
-  def uploader_id
-    post.uploader_id
   end
 
   def self.available_includes

cosbooru/cosbooru

app/models/post_flag.rb
@@ -26,10 +26,12 @@ class PostFlag < ApplicationRecord
     rejected: 2,
   }
 
+  delegate :uploader_id, to: :post
+
   scope :by_users, -> { where.not(creator: User.system) }
   scope :by_system, -> { where(creator: User.system) }
-  scope :in_cooldown, -> { by_users.where("created_at >= ?", Danbooru.config.flag_expiry.ago) }
-  scope :expired, -> { pending.where("post_flags.created_at < ?", Danbooru.config.moderation_period.ago) }
+  scope :in_cooldown, -> { by_users.where(created_at: Danbooru.config.moderation_period.ago..) }
+  scope :expired, -> { pending.where(post_flags: { created_at: ...Danbooru.config.moderation_period.ago }) }
   scope :active, -> { pending.or(rejected.in_cooldown) }
 
   module SearchMethods
@@ -93,12 +95,8 @@ class PostFlag < ApplicationRecord
 
     flag = post.flags.in_cooldown.last
     if !is_deletion && !creator.is_approver? && flag.present?
-      errors.add(:post, "cannot be flagged more than once every #{Danbooru.config.flag_expiry.inspect} (last flagged: #{flag.created_at.to_formatted_s(:long)})")
+      errors.add(:post, "cannot be flagged more than once every #{Danbooru.config.flag_expiry.inspect} (last flagged: #{flag.created_at.to_fs(:long)})")
     end
-  end
-
-  def uploader_id
-    post.uploader_id
   end
 
   def self.available_includes